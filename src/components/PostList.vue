ed4e73126d88da2f97e333516bcf8b866b764ffe
藍天駿 <jay183145@gmail.com>
2022-05-12 15:21:35 +0800

    props in setup function

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="post-list">
     <div v-for="post in posts" :key="post.id">
-      <h3>{{ post.title }}</h3>
+      <SinglePost :post="post" />
     </div>
   </div>
 </template>
 <script>
+import SinglePost from './SinglePost.vue'
 export default {
-  props:['posts'],
+  props: ['posts'],
+  components: { SinglePost },
   setup(props) {
     console.log(props, props.posts)
   }
