bfc96ea609c9e837c2ade267e07a4370c60ca53e
藍天駿 <jay183145@gmail.com>
2022-05-13 13:29:18 +0800

    POST request

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="create">
+  <div class="create" @submit.prevent="handleSubmit">
     <form>
       <label>Title:</label>
       <input v-model="title" type="text" required>
@@ -35,7 +35,21 @@ export default {
       tag.value = ''
     }
 
-    return { title, body, tag, tags, handleKeydown }
+    const handleSubmit = async () => {
+      const post = {
+        title: title.value,
+        body: body.value,
+        tags: tags.value
+      }
+
+      await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(post)
+      })
+    }
+
+    return { title, body, tag, tags, handleKeydown, handleSubmit }
   }
 }
 </script>
