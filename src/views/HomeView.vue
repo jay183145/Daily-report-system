2d3f2dd66fa8bfe8a0d76dbd5509768d2e1c3af1
藍天駿 <jay183145@gmail.com>
2022-05-13 09:56:06 +0800

    composable function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,31 +14,19 @@
 <script>
 import PostList from "../components/PostList.vue";
 import { ref } from '@vue/reactivity'
+import getPosts from "../composables/getPosts";
+
 export default {
   name: 'Home',
   components: { PostList },
   setup() {
-    const posts = ref([])
-    const error = ref(null)
     const showPosts = ref(true)
+    
+    const { posts, error, load } = getPosts()
 
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if (!data.ok) {
-          throw Error('no data available')
-        }
-        posts.value = await data.json()
-        console.log(data)
-      }
-      catch (err) {
-        error.value = err.message
-        console.log(error.value)
-      }
-    }
     load()
 
-    return { posts, showPosts, load, error }
+    return { showPosts, posts, error }
   }
 }
 </script>
