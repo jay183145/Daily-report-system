dc52e6e18ef891ceeb36aad0f305139dd1431e29
藍天駿 <jay183145@gmail.com>
2022-05-13 10:22:37 +0800

    Router wirh composable function

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="post">
+  <router-link :to="{ name: 'Details', params: { id: post.id }}">
     <h3>{{ post.title }}</h3>
+  </router-link>
+  <div class="post">
+    
     <p>{{ snippet }}</p>
     <span v-for="tag in post.tags" :key="tag">
       #{{ tag }}
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Details from '../views/Details.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
-  
+  {
+    path: '/posts/:id',
+    name: 'Details',
+    component: Details,
+    props: true
+  }
 ]
 
 const router = createRouter({
